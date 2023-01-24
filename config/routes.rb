9098f1ccc3e3ad078d3b74a8c2132a93d77eedb7
EC2 Default User <ec2-user@ip-172-31-13-73.ap-northeast-1.compute.internal>
2023-01-24 09:45:10 +0000

    編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new'
   get '/top' =>'homes#top'
-  post 'books' =>'books#create'
-  get 'books' =>'books#index'
-  get '/books/:id' =>'books#show',as: 'book'
-  get 'books/edit'
+  resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_23_135134) do
+ActiveRecord::Schema.define(version: 2023_01_24_083248) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.text "body"
   end
 
 end
