4e815c28b77f51a3a87f60fcfb65e88905e7a062
EC2 Default User <ec2-user@ip-172-31-13-73.ap-northeast-1.compute.internal>
2023-01-25 12:11:09 +0000

    コード完了

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2023_01_21_090129) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
